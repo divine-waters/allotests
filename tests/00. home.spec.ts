854173ef5f6922cd6d203ef8bb13d75f06851cd4
GlickGlickGlick <samarlimitedliabilitycompanyco@gmail.com>
2025-05-21 06:53:42 -0600

    linting

--- tests/00. home.spec.ts
+++ tests/00. home.spec.ts
@@ -4,4 +4,5 @@ test('should load the home page and have the correct title', async ({ page }) =>
   await page.goto('https://www.allocommunications.com/');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/ALLO Fiber/);
+  console.log("Simple Assertion Example Test")
 }); 
